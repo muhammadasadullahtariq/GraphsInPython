2cd58c73acc019246dd7e3bf2ecc9303e7342b97
Asad Ullah <asadullahtariq8995@gmail.com>
2021-04-22 06:41:02 +0500

    Error Removed

--- oneToAllBrodcast.py
+++ oneToAllBrodcast.py
@@ -2,35 +2,40 @@ import math
 
 source = []
 destination = []
-p=0
+p = 0
 
 
 def Log2(x):
     if x == 0:
         return False;
-    return (math.log(x) /
+    return (math.log10(x) /
             math.log10(2))
 def isPowerOfTwo(n):
     return (math.ceil(Log2(n)) ==
             math.floor(Log2(n)));
 def getNumberOfProcess():
-   p=int(input("Enter Number of Process in power 2"))
-   while(not isPowerOfTwo(p)):
-       p = int(input("Enter Again Number of Process in power 2"))
+    global p
+    p = int(input("Enter Number of Process in power 2"))
+    while(not isPowerOfTwo(p)):
+        p = int(input("Enter Again Number of Process in power 2"))
+
 def algorithum():
     source.append(0);
-    r = p/2
+    print(p)
+    r = p//2
+    print(r)
     d = 2
-    for i in range(r):
+    for i in range(r-1):
         for j in range(len(source)):
-            k = d/2
+            k = p//d
             destination.append(source[j]+k)
+        print(str(i) + "\tIteration :")
+        print("Source:\t" + str(source))
+        print("Destination:\t" + str(destination))
         for j in range(len(destination)):
             source.append(destination[j])
         d = d*2
-        print(str(i) + "Iteration :")
-        print("Source:"+source)
-        print("Destination:"+destination)
         destination.clear()
+    print("At The End Source:\t" + str(source))
 getNumberOfProcess()
 algorithum()
